ef2063aa6539c59f19059f181130b8d06402d496
MinyongPark <86906386+Minon98@users.noreply.github.com>
2023-03-07 20:52:31 +0900

    Update dir

---
+++ Inf_study/hello-spring/src/main/java/com/example/hellospring/controller/MemberController.java
@@ -0,0 +1,52 @@
+package com.example.hellospring.controller;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+//    @Autowired (직접 주입 하면 안좋다)
+    private final MemberService memberService;
+
+//    setter로 주입해도 안좋다
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+
+//    생성자 주입이 권장된다
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
