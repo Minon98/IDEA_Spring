472cc4f161ce7e914ffe84825fab2fd810e03370
MinyongPark <86906386+Minon98@users.noreply.github.com>
2023-03-07 15:51:00 +0900

    Update Get Api

--- hello/src/main/java/com/example/hello/controller/GetApiController.java
+++ hello/src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,6 +1,9 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.UserRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
@@ -26,5 +29,48 @@ public class GetApiController {
         return pathName;
     }
 
-    //
+    // http://localhost:9090/api/get/query-param?user=steve&email=steve&gamil.com&age=30
+
+
+    //파라미터가 뭐가 들어올지 모른다면 Map으로 파라미터를 받을 수 있다.
+    @GetMapping(path = "query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam){
+
+        StringBuilder sb = new StringBuilder();
+        queryParam.entrySet().forEach(entry-> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
+
+            sb.append(entry.getKey()+" = "+entry.getValue()+"\n");
+        });
+
+        return sb.toString();
+    }
+
+    // 파라미터를 명시적으로 할 수 있다. 파라미터가 많아지면 불편하다.
+    @GetMapping("/query-param02")
+    public String queryParam02(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam int age
+    ){
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return name+" "+email+" "+age;
+
+    }
+
+    //객체 getter setter로 간단히 가능
+    @GetMapping("/query-param03")
+    public String queryParam03(UserRequest userRequest){
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
+
+    }
 }
