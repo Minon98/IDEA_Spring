486389257997ef88e0f592d3a35899b07d4dcec1
MinyongPark <86906386+Minon98@users.noreply.github.com>
2023-03-13 19:47:03 +0900

    Update filter

--- fastCampus/part3ch06/filter/src/main/java/com/example/filter/filter/GlobalFilter.java
+++ fastCampus/part3ch06/filter/src/main/java/com/example/filter/filter/GlobalFilter.java
@@ -6,13 +6,14 @@ import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 
 @Slf4j
-@Component
+@WebFilter(urlPatterns = "/api/user/*")
 public class GlobalFilter implements Filter {
 
     @Override
@@ -22,16 +23,21 @@ public class GlobalFilter implements Filter {
         ContentCachingRequestWrapper httpServletRequest = new ContentCachingRequestWrapper((HttpServletRequest) request);
         ContentCachingResponseWrapper httpServletResponse = new ContentCachingResponseWrapper((HttpServletResponse) response);
 
-        String url = httpServletRequest.getRequestURI();
-        BufferedReader br = httpServletRequest.getReader();
-        br.lines().forEach(line -> {
-            log.info("url : {}, line : {}", url, line);
-        });
-
         chain.doFilter(httpServletRequest, httpServletResponse);
 
-        //후처리
+        String url = httpServletRequest.getRequestURI();
 
-        
+        //후처리
+        //req
+
+        String reqContent = new String(httpServletRequest.getContentAsByteArray());
+        log.info("request url : {}, request body : {}", url, reqContent);
+
+        String resContent = new String(httpServletResponse.getContentAsByteArray());
+        int httpStatus = httpServletResponse.getStatus();
+
+        httpServletResponse.copyBodyToResponse();
+
+        log.info("response status : {}, responseBody : {}", httpStatus, resContent);
     }
 }
